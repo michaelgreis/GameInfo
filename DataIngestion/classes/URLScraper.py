a6a7430f313d86a4fc5c7042d09185286ad87d2c
michaelgreis <michaelgreis@gmail.com>
2017-08-15 20:43:08 -0500

    Dedup On Search Keywords
    Made the dictionary that was originally not deduped, dedup through the use of a set. Cut down the number of terms (and http requests) on the initial input list by ~50%.

--- DataIngestion/classes/URLScraper.py
+++ DataIngestion/classes/URLScraper.py
@@ -22,36 +22,33 @@ class URLScraper():
         self,filepath,source_name
         ):
         print(time.time())
-        
+        count=0
         for filename in os.listdir(filepath): #loop through all files in the specified directory
             if source_name in filename:
-                 with open(filepath+filename,'r') as file:
+                with open(filepath+filename,'r') as file:
                     for line in file:
                         #line_unsplit = self.remove_non_ascii(line)
                         self.scraper_terms[source_name] = line.split("|")
-
+                file.close()
         print("Scraper configuration successfully read for " + source_name + ".")
         print(time.time())
+        print(len(set(self.scraper_terms[source_name])))
 
 #the purpose of this to take a url which you desire to hit, what string will be altered, and hit the url repeatedly
     def url_scraper(
         self,fullurl,urlreplacestring,source_name,output_location
         ):
-        
         os.chdir(output_location)
-        with open(source_name+'.json','w', encoding='utf-8') as outfile:
-       
+        count=0
+        with open(source_name+str(time.time())+'.json','w', encoding='utf-8') as outfile:
             for key in self.scraper_terms:
-                for item in self.scraper_terms[key]:
+                for item in set(self.scraper_terms[key]):
+                    count+=1
+                    print(count)
                     item = self.remove_non_ascii(item)
                     request_url = fullurl.replace("*",item)
-                    #print(request_url)
                     req = urllib.request.Request(request_url, headers={'User-Agent': "Magic Browser"})
                     with urllib.request.urlopen(req) as response:
                         for line in response:
-                            #decoded = line.decode('latin-1','xmlcharrefreplace') #binascii.b2a_base64(line,newline=True) # #this decoding was chosen at random.
-                            #print(decoded)
                             json.dump(line.decode('utf-8','ignore'),outfile)
-                            #json.dump(line.decode('utf-8','ignore')
-                            # ,outfile)
         outfile.close()
