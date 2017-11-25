4a8373fbbd6dfa56fae3b7dc139ce7c98ae3977a
michaelgreis <michaelgreis@gmail.com>
2017-11-25 11:35:34 -0600

    SonyScraperProduction.py - Cronsettings
    Added the path and other indicators needed by chrontab to run. Mimics what is on the server.

--- DataIngestion/SonyScraperProduction.py
+++ DataIngestion/SonyScraperProduction.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python3.5
+#PATH=/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/loc$
 from classes.SonyScraper import SonyScraper
 from scrapy.crawler import Crawler
 from scrapy.crawler import CrawlerProcess
 import os
 
+print("Starting Scraper")
 source_name='sonymarketplace'
-data_output_location = "OutputFiles/"#"/crawler/data/"
+data_output_location = "/crawler/data/"
+print("Variables Loaded")
 
 os.chdir(data_output_location)
+#print("Directory Changed")
 
 spider=SonyScraper()
 process = CrawlerProcess()
 process.crawl(spider)
 process.start()
+print("Scraper Finished")
+
