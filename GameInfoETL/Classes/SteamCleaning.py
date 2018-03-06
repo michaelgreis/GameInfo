1462543207bc8d82a82f51181e20a291e91b6a11
michaelgreis <michaelgreis@gmail.com>
2018-03-05 21:05:34 -0600

    Start of Data Mart & ETL
    This is the beginning of the datamart and ETL processing jobs.

---
+++ GameInfoETL/Classes/SteamCleaning.py
@@ -0,0 +1,14 @@
+import petl as etl
+import psycopg2
+
+class DataLoader():
+    
+
+    def InsertGame(self,source):
+
+    
+    def InsertMarketEntry(self,source):
+    
+    def InsertSource(self,source):
+    
+    def InsertPublisher(self,source):
