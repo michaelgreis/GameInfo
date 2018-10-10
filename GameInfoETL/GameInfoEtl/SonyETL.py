739c8a0879001ab788f2a3044710c414ec7ce0e8
michaelgreis <michaelgreis@gmail.com>
2018-10-09 20:39:01 -0500

    Release Date fix
    release date now has a default, since nulls are evaluated as <>

--- GameInfoETL/GameInfoEtl/SonyETL.py
+++ GameInfoETL/GameInfoEtl/SonyETL.py
@@ -8,6 +8,6 @@ try:
     EtlTest.InsertMarketEntry('sonymarketplace')
     EtlTest.InsertCategory('sonymarketplace')
     EtlTest.InsertCategoryType('sonymarketplace')
-    EtlTest.InsertMarketitemRelationship('sonymarketplace')
+    #EtlTest.InsertMarketitemRelationship('sonymarketplace') #Commenting out, this was creating a ton of unneccesary data.
 except:
     print('sonymarketplace - failed.'+str(sys.exc_info()[0]))
