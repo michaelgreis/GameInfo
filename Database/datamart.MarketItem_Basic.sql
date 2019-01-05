2024427f439dedb1c765e1fa021dd8feea716976
michaelgreis <michaelgreis@gmail.com>
2019-01-05 15:33:12 -0600

    Fix for Sony Marketplace Entry
    The sonymarketplace entry was inappropriately defaulting the release date to a date datatype when it should have been Varchar(25)

--- Database/datamart.MarketItem_Basic.sql
+++ Database/datamart.MarketItem_Basic.sql
@@ -1,12 +1,14 @@
 --View to be used for basic analysis
-
+--DROP VIEW datamart.marketItem_Basic;
 
 CREATE VIEW datamart.MarketItem_Basic AS
-SELECT mpi.title
+SELECT mpi.marketplaceitemid
+	, mpe.marketentryid
+	, mpi.title
 	, mpi.alternatename
     , mpe.releasedate
     , mpe.primaryprice
-    , mpe.gameimageurl
+    --, mpe.gameimageurl
     , mpe.insertdatetime AS entryupdatetime
     , con.consolename
     , src.sourcename
@@ -16,4 +18,5 @@ ON mpi.marketplaceitemid = mpe.marketplaceitemid
 INNER JOIN datamart.console con
 ON mpi.consoleid = con.consoleid
 INNER JOIN datamart.source src
-ON mpe.sourceid = src.sourceid;
+ON mpe.sourceid = src.sourceid
+WHERE mpi.title IS NOT NULL
